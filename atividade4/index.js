18f029660d952d74ba943b0fd013bf2262ee8b01
HevertSousa <hevert.sousa@outlook.com>
2024-04-08 19:33:46 -0300

    finalizado

--- atividade4/index.js
+++ atividade4/index.js
@@ -1,30 +1,45 @@
+// Atividade 04
+// Desenvolvimento WEB 
+// Hevert Sousa
+// UC19106625
+
 const express = require('express');
 const calculadora = require('./util/calculadora');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// Definição de rota com express
 app.get('/somar/:a/:b', (req, res) => {
+    // Recebendo variáveis da URL
   const { a, b } = req.params;
   const resultado = calculadora.somar(parseFloat(a), parseFloat(b));
   res.send(resultado.toString());
 });
 
+// Definição de rota com express
 app.get('/subtrair/:a/:b', (req, res) => {
+    // Recebendo variáveis da URL
   const { a, b } = req.params;
   const resultado = calculadora.subtrair(parseFloat(a), parseFloat(b));
   res.send(resultado.toString());
 });
 
+// Definição de rota com express
 app.get('/multiplicar/:a/:b', (req, res) => {
-  const { a, b } = req.params;
+    // Recebendo variáveis da URL
+    const { a, b } = req.params;
   const resultado = calculadora.multiplicar(parseFloat(a), parseFloat(b));
   res.send(resultado.toString());
 });
 
+// Definição de rota com express
 app.get('/dividir/:a/:b', (req, res) => {
+    // Recebendo variáveis da URL
   const { a, b } = req.params;
   const resultado = calculadora.dividir(parseFloat(a), parseFloat(b));
+ // Verificando para não haver divisão por 0
   if (isNaN(resultado) || !isFinite(resultado)) {
     res.status(400).send('Divisão por zero ou valor inválido.');
   } else {
@@ -32,6 +47,7 @@ app.get('/dividir/:a/:b', (req, res) => {
   }
 });
 
+// Mostrando a porta em que o servidor está rodando
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
